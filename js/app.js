1b39b4b664430f183a53d9cb96845966c0f0393e
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-02-06 16:08:50 -0800

    wrote the rest of the questions

--- js/app.js
+++ js/app.js
@@ -71,4 +71,107 @@ default:// anything else
   console.log('games:' + games, 'count:' + count);
 }
 
+// How about trees, are they cool? (yes)
+// Store result in trees
+trees = prompt('How about trees, are they cool? Y/Yes or N/No');
+console.log('trees:' + trees);
 
+// Check for string, otherwise store 'no' in trees
+if (typeof(trees) !== 'string') {
+  trees = 'no';
+  console.log('trees:' + trees);
+}
+
+// Trees is a string; make it lowercase; check for 'yes' cases, otherwise
+// store 'no' in trees
+trees = trees.toLowerCase();
+console.log('trees:' + trees);
+switch (trees) {
+case 'y':
+case 'yes':
+  trees = 'yes';
+  count++;
+  console.log('trees:' + trees, 'count:' + count);
+  break;
+default: // anything else
+  trees = 'no';
+  console.log('trees:' + trees, 'count:' + count);
+}
+
+// Would I like a cookie? (yes)
+// Store result in cookie
+cookie = prompt('Would I like a cookie? Y/Yes or N/No');
+console.log('cookie:' + cookie);
+
+// Check if string; otherwise store 'no' in cookie
+if (typeof(cookie) !== 'string') {
+  cookie = 'no';
+  console.log('cookie:' + cookie);
+}
+
+// Make cookie lowercase, check for 'yes' cases, otherwise cookie = 'no'
+cookie = cookie.toLowerCase();
+console.log('cookie:' + cookie);
+switch (cookie) {
+case 'y':
+case 'yes':
+  cookie = 'yes';
+  count++;
+  console.log('cookie:' + cookie, 'count:' + count);
+  break;
+default:
+  cookie = 'no';
+  console.log('cookie:' + cookie, 'count:' + count);
+}
+
+// Is napping an important part of my day? (no)
+// Store result in nap
+nap = prompt('Is napping an important part of my day? Y/Yes or N/No');
+console.log('nap:' + nap);
+
+// If nap is not a string, make nap = 'yes'
+if (typeof(nap) !== 'string') {
+  nap = 'yes';
+  console.log('nap:' + nap);
+}
+
+// Make nap lowercase, check for 'no' cases, increment count
+// otherwise nap = no, do not increment count
+nap = nap.toLowerCase();
+console.log('nap:' + nap);
+switch (nap) {
+case 'n':
+case 'no':
+  nap = 'no';
+  count++;
+  console.log('nap:' + nap, 'count:' + count);
+  break;
+default:
+  nap = 'yes';
+  console.log('nap:' + nap, 'count:' + count);
+}
+
+// Coding is cool, right? (yes)
+coding = prompt('Coding is cool, right? Y/Yes or N/No');
+console.log('coding:' + coding);
+
+// If coding is not a string, make coding = 'no'
+if (typeof(coding) !== 'string') {
+  coding = 'no';
+  console.log('coding:' + coding);
+}
+
+// Make coding lowercase, check for 'yes' cases, otherwise coding = no
+coding = coding.toLowerCase();
+console.log('coding:' + coding);
+switch (coding) {
+case 'y':
+case 'yes':
+  coding = 'yes';
+  count++;
+  console.log('coding:' + coding, 'count:' + count);
+  break;
+default:
+  coding = 'no';
+  console.log('coding:' + coding, 'count:' + count);
+}
